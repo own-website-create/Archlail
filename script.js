1b508b16282f45b8712757a5a06cc2afffa1f6b0
Web <web.site.programdesigner@gmail.com>
2026-06-12 00:44:32 +0330

    Initial commit

--- script.js
+++ script.js
@@ -1,3 +1,28 @@
+const images = document.querySelectorAll(".gallery-image");
+
+const lightbox = document.getElementById("lightbox");
+
+const lightboxImg = document.getElementById("lightbox-img");
+
+const closeBtn = document.getElementById("close-btn");
+
+images.forEach(img => {
+
+    img.addEventListener("click", () => {
+
+        lightbox.style.display = "flex";
+
+        lightboxImg.src = img.src;
+    });
+
+});
+
+closeBtn.addEventListener("click", () => {
+
+    lightbox.style.display = "none";
+
+});
+
 function toggleMenu(){
     let menu = document.getElementById("menu");
 
